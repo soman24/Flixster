49ecfefa963c11881bc83cd61883294a2b1ec850
soman24 <khansom@oregonstate.edu>
2021-10-04 21:26:45 -0500

    Part 2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Soman Khan on 10/3/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
